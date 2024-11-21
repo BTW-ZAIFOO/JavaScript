881b1eccb3c706257fca92847fbbe606aec63aa0
Huzaifa Khan <huzaifazaifi25@gmail.com>
2024-11-21 13:01:00 +0500

    largest num in array using loops.js

---
+++ largest num in array using loops.js
@@ -0,0 +1,11 @@
+let array =[1,4,5,78,65,34,98]
+
+let maximum=0;
+for (let i=0; i<array.length ; i++){
+
+if(array[i]>maximum){
+maximum=array[i];
+
+}
+}
+console.log("maximum number is:" + maximum)
