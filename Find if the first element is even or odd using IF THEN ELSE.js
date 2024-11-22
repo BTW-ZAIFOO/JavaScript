d66d0476cac3348a8a4cc6f5b7891a42d656ed27
Huzaifa Khan <huzaifazaifi25@gmail.com>
2024-11-22 16:40:00 +0500

    Find if the first element is even or odd using IF THEN ELSE.js

---
+++ Find if the first element is even or odd using IF THEN ELSE.js
@@ -0,0 +1,14 @@
+const arr = [7, 2, 3]
+
+if (arr[0] % 2 === 0)
+{
+  console.log("The first element is even.")
+}
+else
+{
+  console.log("The first element is odd.")
+}
+
+//-----OUTPUT------//
+
+"The first element is odd."
