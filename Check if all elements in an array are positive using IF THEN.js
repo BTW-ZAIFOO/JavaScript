f1df091e2d0ea92e8a0862e33731029eb664c9d9
Huzaifa Khan <huzaifazaifi25@gmail.com>
2024-11-22 16:24:23 +0500

    Check if all elements in an array are positive using IF THEN.js

---
+++ Check if all elements in an array are positive using IF THEN.js
@@ -0,0 +1,10 @@
+const arr = [1, 2, 3, 4]
+
+if (arr.every((num) => num > 0))
+{
+  console.log("All elements in the array are positive.")
+}
+
+//-------OUTPUT--------//
+
+"All elements in the array are positive."
