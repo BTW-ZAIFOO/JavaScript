4d6254ad8feba8dec5dbac3abecdac270e567132
Huzaifa Khan <huzaifazaifi25@gmail.com>
2024-11-22 13:50:33 +0500

    Find the product of all elements using FOR LOOP.js

---
+++ Find the product of all elements using FOR LOOP.js
@@ -0,0 +1,13 @@
+const arr = [1, 2, 3, 4]
+
+let product = 1
+
+for (let i = 0; i < arr.length; i++) {
+  product *= arr[i]
+}
+
+console.log("Product:", product)
+
+//----OUTPUT-----//
+
+"Product:", 24
