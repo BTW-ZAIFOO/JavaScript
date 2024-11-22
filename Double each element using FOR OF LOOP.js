dec2f5f4e3d30f855244d6980473f18e44a99022
Huzaifa Khan <huzaifazaifi25@gmail.com>
2024-11-22 14:03:20 +0500

    Double each element using FOR OF LOOP.js

---
+++ Double each element using FOR OF LOOP.js
@@ -0,0 +1,13 @@
+const arr = [1, 2, 3]
+
+const doubled = []
+
+for (const num of arr) {
+  doubled.push(num * 2)
+}
+
+console.log("Doubled Array:", doubled)
+
+//-----OUTPUT------//
+
+"Doubled Array:", [2, 4, 6]
