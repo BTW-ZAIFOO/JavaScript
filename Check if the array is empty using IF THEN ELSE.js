de8ee8d2b80e90ce5a1b360fa638b6d5168380d7
Huzaifa Khan <huzaifazaifi25@gmail.com>
2024-11-22 16:36:14 +0500

    Check if the array is empty using IF THEN ELSE.js

---
+++ Check if the array is empty using IF THEN ELSE.js
@@ -0,0 +1,14 @@
+const arr = []
+
+if (arr.length === 0)
+{
+  console.log("The array is empty.")
+} 
+else 
+{
+  console.log("The array has elements.")
+}
+
+//-----OUTPUT-----//
+
+"The array is empty."
